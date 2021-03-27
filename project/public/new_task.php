de61f22d426f8d7c66da68461bc46a180c1d1c10
Jeff <jeff.decena@yahoo.com>
2021-03-28 07:20:34 +1300

    part 2 - worker queues

--- project/public/new_task.php
+++ project/public/new_task.php
@@ -8,9 +8,14 @@ use PhpAmqpLib\Message\AMQPMessage;
 $connection = new AMQPStreamConnection('rabbitmq', 5672, 'guest', 'guest');
 $channel = $connection->channel();
 
-$channel->queue_declare('hello', false, false, false, false);
+$channel->queue_declare('task_queue', false, true, false, false);
 
-$msg = new AMQPMessage('Hello World!');
+$data = implode(' ', array_slice($argv, 1));
+if (empty($data)) {
+    $data = "Hello World!";
+}
+
+$msg = new AMQPMessage($data, ['delivery_mode' => AMQPMessage::DELIVERY_MODE_PERSISTENT]);
 $channel->basic_publish($msg, '', 'hello');
 
 echo " [x] Sent 'Hello World!'\n";
