55434138bbfaf7ddab7f78ba09689dde9d838d54
Jeff <jeff.decena@yahoo.com>
2021-03-28 12:27:50 +1300

    part 3 - Pub sub

--- project/public/emit_log.php
+++ project/public/emit_log.php
@@ -4,21 +4,20 @@ require_once __DIR__ . '/../vendor/autoload.php';
 use PhpAmqpLib\Connection\AMQPStreamConnection;
 use PhpAmqpLib\Message\AMQPMessage;
 
-// var_dump(getenv('RABBITMQ_HOST')); die;
 $connection = new AMQPStreamConnection('rabbitmq', 5672, 'guest', 'guest');
 $channel = $connection->channel();
 
-$channel->queue_declare('task_queue', false, true, false, false);
+$channel->exchange_declare('logs', 'fanout', false, false, false);
 
 $data = implode(' ', array_slice($argv, 1));
 if (empty($data)) {
-    $data = "Hello World!";
+    $data = "info: Hello World!";
 }
+$msg = new AMQPMessage($data);
 
-$msg = new AMQPMessage($data, ['delivery_mode' => AMQPMessage::DELIVERY_MODE_PERSISTENT]);
-$channel->basic_publish($msg, '', 'hello');
+$channel->basic_publish($msg, 'logs');
 
-echo " [x] Sent 'Hello World!'\n";
+echo ' [x] Sent ', $data, "\n";
 
 $channel->close();
 $connection->close();
